00014b46bfaaf29cc6a44e8b5b19c038d7773d2f
FernandoAndradas <129375052+FernandoAndradas@users.noreply.github.com>
2023-05-25 12:54:32 +0200

    Clase servicio catering y su metodo

--- src/main/java/com/mycompany/bitedelivery/ServicioCatering.java
+++ src/main/java/com/mycompany/bitedelivery/ServicioCatering.java
@@ -10,55 +10,106 @@ package com.mycompany.bitedelivery;
  */
 public class ServicioCatering {
     //ATRIBUTOS
-    private int comida_proporcionar;
-    private int servicios;
-    private int espacio;
-    private int total;
+    private boolean comida_proporcionar;
+    private boolean camareros;
+    private boolean cocineros;
+    private boolean decoracion;
+    private boolean espacio;
+    private double total;
     //CONSTRUCTOR
 
-    public ServicioCatering(int comida_proporcionar, int servicios, int espacio, int total) {
+    public ServicioCatering(boolean comida_proporcionar, boolean camareros, boolean cocineros, boolean decoracion, boolean espacio, double total) {
         this.comida_proporcionar = comida_proporcionar;
-        this.servicios = servicios;
+        this.camareros = camareros;
+        this.cocineros = cocineros;
+        this.decoracion = decoracion;
         this.espacio = espacio;
         this.total = total;
     }
     
-    //GETTERS AND SETTERS
-    public int getTotal() {
+    
+    
+    
+    
+     //GETTERS AND SETTERS
+    public boolean isDecoracion() {
+        return decoracion;
+    }
+
+    public void setDecoracion(boolean decoracion) {
+        this.decoracion = decoracion;
+    }
+
+    public boolean isCocineros() {
+        return cocineros;
+    }
+
+    public void setCocineros(boolean cocineros) {
+        this.cocineros = cocineros;
+    }
+
+    public boolean isCamareros() {
+        return camareros;
+    }
+
+    public void setCamareros(boolean camareros) {
+        this.camareros = camareros;
+    }
+    
+    public double getTotal() {
         return total;
     }
 
-    public void setTotal(int total) {
+    public void setTotal(double total) {
         this.total = total;
     }
 
-    public int getEspacio() {
+    public boolean getEspacio() {
         return espacio;
     }
 
-    public void setEspacio(int espacio) {
+    public void setEspacio(boolean espacio) {
         this.espacio = espacio;
     }
 
-    public int getServicios() {
-        return servicios;
-    }
 
-    public void setServicios(int servicios) {
-        this.servicios = servicios;
-    }
-
-    public int getComida_proporcionar() {
+    public boolean getComida_proporcionar() {
         return comida_proporcionar;
     }
 
-    public void setComida_proporcionar(int comida_proporcionar) {
+    public void setComida_proporcionar(boolean comida_proporcionar) {
         this.comida_proporcionar = comida_proporcionar;
     }
 
     @Override
     public String toString() {
-        return "ServicioCatering{" + "comida_proporcionar=" + comida_proporcionar + ", servicios=" + servicios + ", espacio=" + espacio + ", total=" + total + '}';
+        return "ServicioCatering{" + "comida_proporcionar=" + comida_proporcionar + ", camareros=" + camareros + ", cocineros=" + cocineros + ", decoracion=" + decoracion + ", espacio=" + espacio + ", total=" + total + '}';
     }
 
+
+    //METODOS
+    public void Costetotal( boolean comida_proporcionar,boolean camareros,boolean cocineros,boolean decoracion,boolean espacio){
+        double total =this.total;
+        if (this.comida_proporcionar==true){
+       total = total+ 1000;
+    }
+        if (this.camareros==true){
+       total = total + 800; 
+    }
+          if (this.cocineros==true){
+       total = total + 1000; 
+    }
+            if (this.decoracion==true){
+       total = total + 500; 
+    }
+              if (this.espacio==true){
+       total = total + 1000; 
+    }
+    
+    }
+    
+    
+    
+    
+    
 }
